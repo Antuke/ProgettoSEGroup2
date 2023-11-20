07e29b69afa7fa77fd4e8038e15d49483a2e5ef3
Antuke <139248337+Antuke@users.noreply.github.com>
2023-11-20 14:51:57 +0100

    Creazione interfaccia main e il suo skeleton controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,6 @@ module com.segroup2.progettosegroup2 {
 
 
     opens com.segroup2.progettosegroup2 to javafx.fxml;
+    opens com.segroup2.progettosegroup2.Controllers to javafx.fxml;
     exports com.segroup2.progettosegroup2;
 }
