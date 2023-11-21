de69109dbfee8bdc923a292371bf79e2e98e620f
MassimilianoRanauro <m.ranauro2@studenti.unisa.it>
2023-11-21 12:16:23 +0100

    Creazione della classe ActionAudio
    Creazione della classe ActionAudio
    Creazione della classe di test ActionAudioTest e relativo package
    Aggiornamento dei file pom.xml e module-info.java per aggiornare le dipendenze necessarie

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.segroup2.progettosegroup2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
 
     opens com.segroup2.progettosegroup2 to javafx.fxml;
