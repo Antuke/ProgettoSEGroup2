37e8c6df67e3116dbcf05a9a5f8d2faca9af8bf0
Pietro <115733985+pietroemme@users.noreply.github.com>
2023-11-23 00:25:46 +0100

    Scrittura di ActionDialogBox.java in JavaFX senza l'utilizzo di FXML.

--- src/main/java/com/segroup2/progettosegroup2/Actions/ActionDialogBox.java
+++ src/main/java/com/segroup2/progettosegroup2/Actions/ActionDialogBox.java
@@ -1,30 +1,66 @@
 package com.segroup2.progettosegroup2.Actions;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class ActionDialogBox extends Application implements ActionInterface {
+
+    private Stage stage;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ActionDialogBox.class.getResource("/com/segroup2/progettosegroup2/dialog-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
-        stage.setTitle("DialogBoxMessage");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        AnchorPane anchorPane = new AnchorPane();
+        anchorPane.setMinWidth(400);
+        anchorPane.setMinHeight(250);
+
+        Label lblMessageBody = new Label("Orario del giorno");
+        AnchorPane.setTopAnchor(lblMessageBody, 100.0);
+        AnchorPane.setLeftAnchor(lblMessageBody, 0.0);
+        AnchorPane.setRightAnchor(lblMessageBody, 0.0);
+        lblMessageBody.setAlignment(Pos.CENTER);
+
+        Label lblMessageTitle = new Label("Messaggio di Avviso");
+        AnchorPane.setTopAnchor(lblMessageTitle, 50.0);
+        AnchorPane.setLeftAnchor(lblMessageTitle, 0.0);
+        AnchorPane.setRightAnchor(lblMessageTitle, 0.0);
+        lblMessageTitle.setAlignment(Pos.CENTER);
+
+        Button btnClose = new Button("Chiudi");
+        AnchorPane.setTopAnchor(btnClose, 150.0);
+        AnchorPane.setLeftAnchor(btnClose, 0.0);
+        AnchorPane.setRightAnchor(btnClose, 0.0);
+        btnClose.setPrefWidth(350.0);
+        btnClose.setOnMouseClicked(event -> btnCloseClicked());
+
+        anchorPane.getChildren().addAll(lblMessageBody, lblMessageTitle, btnClose);
+
+        Scene scene = new Scene(anchorPane, 400, 250);
+        primaryStage.setTitle("DialogBox di Avviso");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
+    private void btnCloseClicked() {
+        stage.close(); //Invoco la close sullo stage, Platform.exit(); non funziona in quanto chiude anche il programma principale
+    }
+
+
+    //Ovviamente il main è da eliminare, ma, finchè si prevedono modifiche all'interfaccia, lo stesso verrà preservato ai fini di test.
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
-    @Override
     public boolean execute() {
-        launch();
+        stage = new Stage(); //Ad ogni richiesta di esecuzione creo un nuovo stage.
+        this.start(stage);
         return true;
     }
 }
+
+
 
