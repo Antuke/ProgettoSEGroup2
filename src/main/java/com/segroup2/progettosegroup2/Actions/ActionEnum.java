15addafd1770693b9e909572cc4759b89abbee90
MassimilianoRanauro <m.ranauro2@studenti.unisa.it>
2023-11-30 17:25:43 +0100

    Aggiornamento add-rule-box.fxml
    Aggiornamento dell'interfaccia grafica con l'aggiunta dell'azione di copia di un file.
    Aggiornata la classe ActionEnum con il valore ACTION_COPY_FILE

--- src/main/java/com/segroup2/progettosegroup2/Actions/ActionEnum.java
+++ src/main/java/com/segroup2/progettosegroup2/Actions/ActionEnum.java
@@ -4,14 +4,8 @@ package com.segroup2.progettosegroup2.Actions;
  * Possible choices for creating objects of ActionInterface
  */
 public enum ActionEnum {
-    /**
-     * Generates an object of the ActionAudio class
-     */
     ACTION_DEFAULT_AUDIO,
-    /**
-     * Generates an object of the ActionDialogBox class
-     */
     ACTION_DEFAULT_DIALOGBOX,
-
-    ACTION_APPEND_TO_FILE
+    ACTION_APPEND_TO_FILE,
+    ACTION_COPY_FILE
 }
