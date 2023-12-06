c829cbf7aece263927c2a784bcc9d088cbb7573a
MassimilianoRanauro <m.ranauro2@studenti.unisa.it>
2023-12-06 16:40:50 +0100

    Semplificazione lettura test di ActionCopyFileTest

--- src/test/java/com/segroup2/progettosegroup2/Actions/ActionCopyFileTest.java
+++ src/test/java/com/segroup2/progettosegroup2/Actions/ActionCopyFileTest.java
@@ -1,22 +1,19 @@
 package com.segroup2.progettosegroup2.Actions;
 
-import com.segroup2.progettosegroup2.MainApplication;
 import com.segroup2.progettosegroup2.TestFX;
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.nio.file.Files;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ActionCopyFileTest extends TestFX {
-    private File base;
     private File fileExists;
     private File fileExistsOtherDirectory;
     private File otherFileExists;
@@ -26,40 +23,27 @@ class ActionCopyFileTest extends TestFX {
     private File directoryNotExists;
 
     @BeforeAll
-    void createFiles() throws URISyntaxException, IOException {
-        base= new File( MainApplication.class.getResource("").toURI().resolve("ActionCopyFileTest") );
-        FileUtils.deleteDirectory(base);
-        base.mkdir();
+    void createFiles() throws IOException {
+        File source= Files.createTempDirectory("Test_source").toFile();
 
-        File source= base.toPath().resolve("Test_source").toFile();
-        source.mkdir();
+        directoryExists= Files.createTempDirectory("Test_destination_exists").toFile();
 
-        directoryExists= base.toPath().resolve("Test_destination_exists").toFile();
-        directoryExists.mkdir();
+        otherDirectoryExists= Files.createTempDirectory("Test_destination_exists_other").toFile();
 
-        otherDirectoryExists= base.toPath().resolve("Test_destination_exists_other").toFile();
-        otherDirectoryExists.mkdir();
+        directoryNotExists= Files.createTempDirectory("Test_destination_not_exists").toFile();
+        FileUtils.deleteDirectory(directoryNotExists);
 
-        directoryNotExists= base.toPath().resolve("Test_destination_not_exists").toFile();
+        String fileExistsName= "Test_file_exists_empty";
+        String fileExistsExtension= ".txt";
+        fileExists= File.createTempFile(fileExistsName, fileExistsExtension, source);
 
-        fileExists = source.toPath().resolve("Test_file_exists_empty.txt").toFile();
-        fileExists.createNewFile();
+        File otherDirectory= Files.createTempDirectory(source.toPath(), "Other directory").toFile();
 
-        File otherDirectory= source.toPath().resolve("Other directory").toFile();
-        otherDirectory.mkdir();
+        fileExistsOtherDirectory= File.createTempFile(fileExistsName, fileExistsExtension, otherDirectory);
 
-        fileExistsOtherDirectory= otherDirectory.toPath().resolve(fileExists.getName()).toFile();
-        fileExistsOtherDirectory.createNewFile();
-
-        otherFileExists = source.toPath().resolve("Test_file_exists_other_empty.txt").toFile();
-        otherFileExists.createNewFile();
+        otherFileExists= File.createTempFile("Test_file_exists_other_empty", ".txt", source);
 
         fileNotExists= source.toPath().resolve("Test_file_not_exists_empty.txt").toFile();
-    }
-
-    @AfterAll
-    void deleteAll() throws IOException {
-        FileUtils.deleteDirectory(base);
     }
 
     @Test
