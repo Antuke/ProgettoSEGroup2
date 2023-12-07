1f2f3a822f64e150dc582a9f8ad715c0f02fc0ce
MassimilianoRanauro <m.ranauro2@studenti.unisa.it>
2023-12-08 00:26:16 +0100

    Aggiunta di DayOfWeekDecorator
    Aggiunta della classe DayOfWeekDecorator per consentire la scrittura in una lingua specifica dei giorni della settimana.
    Creazione dei test e aggiornamento di RenderTriggerDayOfWeek

--- src/main/java/com/segroup2/progettosegroup2/Controllers/RenderTriggerState/RenderTriggerDayOfWeek.java
+++ src/main/java/com/segroup2/progettosegroup2/Controllers/RenderTriggerState/RenderTriggerDayOfWeek.java
@@ -1,5 +1,6 @@
 package com.segroup2.progettosegroup2.Controllers.RenderTriggerState;
 
+import com.segroup2.progettosegroup2.Triggers.DayOfWeekDecorator;
 import com.segroup2.progettosegroup2.Triggers.TriggerDayOfWeek;
 import com.segroup2.progettosegroup2.Triggers.TriggerInterface;
 import javafx.collections.FXCollections;
@@ -8,8 +9,6 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.time.DayOfWeek;
-
 /**
  * Classe per la corretta visualizzazione e scelta di un oggetto {@link TriggerDayOfWeek}
  */
@@ -17,12 +16,12 @@ public class RenderTriggerDayOfWeek implements RenderTrigger{
     private TriggerDayOfWeek trigger = null;
     @Override
     public void render(VBox parent) {
-        ComboBox<DayOfWeek> list = new ComboBox<>();
+        ComboBox<String> list = new ComboBox<>();
         list.setPromptText("Choose day");
-        list.setItems(FXCollections.observableArrayList(DayOfWeek.values()));
+        list.setItems(FXCollections.observableArrayList(DayOfWeekDecorator.values()));
         Button addTriggerBtn = new Button("Add Trigger");
         addTriggerBtn.setOnAction(e->{
-            trigger = new TriggerDayOfWeek(list.getValue());
+            trigger = new TriggerDayOfWeek( DayOfWeekDecorator.parseFromString(list.getValue()) );
             ((Stage) addTriggerBtn.getScene().getWindow()).close();
         });
         addTriggerBtn.disableProperty().bind(list.valueProperty().isNull());
