580792ec6485124714da84b2b657605a80b5f1c1
MassimilianoRanauro <m.ranauro2@studenti.unisa.it>
2023-12-08 19:56:30 +0100

    Update RenderActionSetCounter.java
    Inserimento commenti + cancellazione import inutilizzati

--- src/main/java/com/segroup2/progettosegroup2/Controllers/RenderActionsState/RenderActionSetCounter.java
+++ src/main/java/com/segroup2/progettosegroup2/Controllers/RenderActionsState/RenderActionSetCounter.java
@@ -10,7 +10,6 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -19,31 +18,36 @@ public class RenderActionSetCounter implements RenderAction {
 
     @Override
     public void render(VBox parent) {
+        // Elemento per la selezione del counter
         ComboBox<Counter> counterComboBox= new ComboBox<>();
         counterComboBox.setItems(CountersManager.getInstance().getCounters());
         counterComboBox.setPromptText("Choose counter");
 
+        // Elemento per la scelta del valore
         TextField value= new TextField();
         value.setPromptText("Value");
         value.setAlignment(Pos.CENTER);
         value.textProperty().addListener((observable, oldValue, newValue) -> {
-            if ( !newValue.matches("[-]?\\d*") ){
+            if ( !newValue.matches("-?\\d*") ){
                 value.setText(oldValue);
             }
         });
 
+        // Pulsante di aggiunta dell'azione
         Button addButton= new Button("Add action");
         addButton.setOnAction( (ActionEvent e) -> {
             action= new ActionSetCounter( counterComboBox.getValue(), Integer.parseInt(value.getText()) );
             ( (Stage) addButton.getScene().getWindow() ).close();
         });
 
+        // Allineamento elementi
         VBox box= new VBox();
         box.setAlignment(Pos.CENTER);
         box.setSpacing(10);
         box.setPadding(new Insets(55));
         box.getChildren().addAll(counterComboBox, value, addButton);
 
+        // Disabilita il pulsante in certe condizioni + aggiunta nodi a parent
         addButton.disableProperty().bind( counterComboBox.valueProperty().isNull().or( value.textProperty().isEmpty() ) );
         parent.getChildren().addAll(box);
     }
